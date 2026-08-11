69072f743b52aac589c892192bc5f26288f7371c
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2026-08-11 13:46:17 +0700

    install zod for validation

--- node_modules/.pnpm/node_modules/@chatapp/auth-service/src/index.ts
+++ node_modules/.pnpm/node_modules/@chatapp/auth-service/src/index.ts
@@ -1,8 +1,14 @@
+import { createApp } from "./app.js";
+import { createServer } from "http";
+
 const main = async()=> {
     try {
-        
+        const app = createApp();
+        const server = createServer(app);
+
+        const port = 
     } catch (error) {
-        
+
     }
 }
 
