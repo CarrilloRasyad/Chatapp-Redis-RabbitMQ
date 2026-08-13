33b9930cbcffdc6dcb4f1526ea71a43f0f420301
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2026-08-13 14:27:35 +0700

    fixing logger info for running on port auth service

--- services/auth-service/src/index.ts
+++ services/auth-service/src/index.ts
@@ -1,16 +1,22 @@
 import { createApp } from "@/app.js";
 import { createServer } from "http";
 import { env } from "@/config/env.js";
+import { logger } from "@/utils/logger.js";
 
 const main = async()=> {
     try {
         const app = createApp();
         const server = createServer(app);
 
-        const port = env.AUTH_SERVICE_PORT
-    } catch (error) {
+        const port = env.AUTH_SERVICE_PORT;
 
+        server.listen(port, () => {
+            logger.info({port}, "Auth service is running");
+        });
+    } catch (error) {
+        logger.error({error}, "Failed to start auth service");
+        process.exit(1);
     }
 }
 
-main();
+void main();
