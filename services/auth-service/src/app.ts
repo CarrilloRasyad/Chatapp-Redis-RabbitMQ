bab2754bd4fc4bb8841d0d6110c354ffef83c625
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2026-08-21 14:45:47 +0700

    fixing app.ts

--- services/auth-service/src/app.ts
+++ services/auth-service/src/app.ts
@@ -1,6 +1,7 @@
 import express, {type Application} from "express";
 import cors from 'cors';
 import helmet from 'helmet';
+import { errorHandler } from "./middleware/error-handler.js";
 
 
 export const createApp = (): Application => {
@@ -19,6 +20,8 @@ export const createApp = (): Application => {
     app.use((_req, res) => {
         res.status(404).json({message: "Not found"});
     });
+
+    app.use(errorHandler);
 
     return app;
 };
