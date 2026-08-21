dc8af1a04523a85f8e4d682809798953d460484d
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2026-08-21 15:21:16 +0700

    run build

--- packages/common/dist/index.d.ts
+++ packages/common/dist/index.d.ts
@@ -1,6 +1,6 @@
 export * from "./env.js";
 export * from "./logger.js";
-export * from "./errors/http-errors.js";
+export * from "./errors/http-error.js";
 export { z } from "zod";
 export type { Logger } from "pino";
 //# sourceMappingURL=index.d.ts.map
