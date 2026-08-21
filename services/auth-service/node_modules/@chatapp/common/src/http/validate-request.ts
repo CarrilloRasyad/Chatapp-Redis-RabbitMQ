6a4093a1069b8411234413e9dee63299958ac887
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2026-08-21 15:51:26 +0700

    install express on packages/common

---
+++ services/auth-service/node_modules/@chatapp/common/src/http/validate-request.ts
@@ -0,0 +1,6 @@
+import {z} from "zod";
+import { HttpError } from "../errors/http-error.js";
+
+import type {NextFunction, Request, Response} from "express";
+import type {ZodError, ZodType} from "zod";
+
