3676143cec74cb4c132e09b88b1681f6850df157
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2026-08-26 17:03:19 +0700

    add: validate request auth service

--- packages/common/src/http/validate-request.ts
+++ packages/common/src/http/validate-request.ts
@@ -2,5 +2,14 @@ import {z} from "zod";
 import { HttpError } from "../errors/http-error.js";
 
 import type {NextFunction, Request, Response} from "express";
-import type {ZodError, ZodType} from "zod";
+import type { AnyZodObject, ZodError, ZodTypeAny } from "zod";
 
+type Schema = AnyZodObject | ZodTypeAny;
+type ParamsRecord = Record<string, string>;
+type QueryRecord = Record<string, unknown>;
+
+export interface RequestValidationSchemas {
+    body?: Schema;
+    params?: Schema;
+    query?: Schema;
+}
