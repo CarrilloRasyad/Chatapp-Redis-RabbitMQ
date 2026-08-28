80b419ef9c21ba9d0e8a8948186d8d22ee8820f6
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2026-08-28 14:46:08 +0700

    fix: validate request SSOT

--- services/auth-service/node_modules/@chatapp/common/src/http/validate-request.ts
+++ services/auth-service/node_modules/@chatapp/common/src/http/validate-request.ts
@@ -1,8 +1,8 @@
-import {z} from "zod";
+// import {z} from "zod";
 import { HttpError } from "../errors/http-error.js";
 
 import type {NextFunction, Request, Response} from "express";
-import type { AnyZodObject, ZodError, ZodTypeAny } from "zod";
+import { AnyZodObject, ZodError, ZodTypeAny } from "zod";
 
 type Schema = AnyZodObject | ZodTypeAny;
 type ParamsRecord = Record<string, string>;
@@ -13,3 +13,42 @@ export interface RequestValidationSchemas {
     params?: Schema;
     query?: Schema;
 }
+
+const formatedError = (error: ZodError) => 
+    error.errors.map((issue) => ({
+        path: issue.path.join("."),
+        message: issue.message
+    }));
+
+export const validateRequest = (schemas: RequestValidationSchemas) => {
+    return (req: Request, _res: Response, next: NextFunction) => {
+        try{
+            if(schemas.body) {
+                const parseBody = schemas.body.parse(req.body) as unknown;
+                req.body = parseBody;
+            }
+
+            if(schemas.params) {
+                const parseParams = schemas.params.parse(req.params) as ParamsRecord;
+                req.params = parseParams as Request["params"];
+            }
+
+            if(schemas.query) {
+                const parseQuery = schemas.query.parse(req.query) as QueryRecord;
+                req.query = parseQuery as Request["query"];
+            }
+
+            next();
+        } catch (error) {
+            if(error instanceof ZodError) {
+                next(
+                    new HttpError(422, "Validation error", {
+                        issues: formatedError(error),
+                    }),
+                );
+                return;
+            }
+            next(error);
+        }
+    };
+};
